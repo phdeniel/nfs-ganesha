be7e52cb2b70797516d1e11edb30f23395af85c6
Jim Lieb <jlieb@panasas.com>
2012-04-16 10:13:21 -0700

    Remove old FSAL api definitions and deprecate old VFS source
    first pass.  squashing a cleanup/completion is in order
    
    Signed-off-by: Jim Lieb <jlieb@panasas.com>

--- src/include/fsal.h
+++ src/include/fsal.h
@@ -341,6 +341,8 @@ fsal_status_t FSAL_name2buffdesc(fsal_name_t * in_name, fsal_buffdesc_t * out_bu
 int FSAL_handlecmp(fsal_handle_t * handle1, fsal_handle_t * handle2,
                    fsal_status_t * status);
 
+#if 0
+
 #ifndef _USE_SWIG
 
 /**
@@ -726,6 +728,8 @@ fsal_status_t FSAL_lock_op( fsal_file_t       * p_file_descriptor,   /* IN */
                             fsal_lock_param_t * conflicting_lock     /* OUT */
                             );
 
+#endif
+
 /* FSAL_UP functions */
 /* These structs are defined here because including fsal_up.h causes
  * preprocessor issues. */
@@ -770,6 +774,8 @@ typedef struct fsal_xattrent__
   fsal_attrib_list_t attributes;         /**< entry attributes (if supported) */
 
 } fsal_xattrent_t;
+
+#if 0
 
 /**
  * Retrieves the list of extended attributes for an object in the filesystem.
@@ -956,6 +962,8 @@ fsal_status_t FSAL_check_quota( char * path,  /* IN */
 unsigned int FSAL_GetFileno(fsal_file_t * pfile);
 #define FSAL_FILENO( pfile ) FSAL_GetFileno( pfile )
 
+#endif
+
 /**
  * fsal2unix_mode:
  * Convert FSAL mode to posix mode.
@@ -980,6 +988,8 @@ fsal_dev_t posix2fsal_devt(dev_t posix_devid);
  */
 fsal_accessmode_t unix2fsal_mode(mode_t unix_mode);
 
+#if 0
+
 /* The following functions are used in Cache_Inode_Asynch mode */
 
 fsal_status_t FSAL_setattr_access(fsal_op_context_t * p_context,        /* IN */
@@ -1003,6 +1013,8 @@ fsal_status_t FSAL_create_access(fsal_op_context_t * pcontext,  /* IN */
 
 fsal_status_t FSAL_link_access(fsal_op_context_t * pcontext,    /* IN */
                                fsal_attrib_list_t * pattr);     /* IN */
+
+#endif
 
 /******************************************************
  *                Structure used to define a fsal
