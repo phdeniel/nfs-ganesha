fb1d388071555f07a2569498c04d26f11a8b77a6
Philippe DENIEL <philippe.deniel@cea.fr>
2012-07-24 15:53:14 +0200

    9P: RDMA logic now involves the dispatch/worker logic
    Signed-off-by: Philippe DENIEL <philippe.deniel@cea.fr>

--- src/include/nfs_core.h
+++ src/include/nfs_core.h
@@ -430,7 +430,8 @@ typedef enum request_type__
   NFS_CALL,
   NFS_REQUEST,
   NFS_REQUEST_LEADER,
-  _9P_REQUEST
+  _9P_REQUEST,
+  _9P_RDMA
 } request_type_t ;
 
 typedef struct request_data__
@@ -444,6 +445,9 @@ typedef struct request_data__
         nfs_request_data_t *nfs ;
 #ifdef _USE_9P
         _9p_request_data_t _9p ;
+#endif
+#ifdef _USE_9P_RDMA
+        _9p_rdma_request_data_t _9p_rdma ;
 #endif
     } r_u ;
 } request_data_t ;
