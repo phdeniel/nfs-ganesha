0482eb496abb62559028a69487e4d90a502701fe
Philippe DENIEL <philippe.deniel@cea.fr>
2013-07-12 10:08:44 +0200

    Stackable FSALs: fix a few warning observed as CMAKE_BUILD_TYPE=Maintainer

--- src/FSAL/Stackable_FSALs/FSAL_NULL/export.c
+++ src/FSAL/Stackable_FSALs/FSAL_NULL/export.c
@@ -236,50 +236,6 @@ void nullfs_export_ops_init(struct export_ops *ops)
 
 void nullfs_handle_ops_init(struct fsal_obj_ops *ops);
 
-/* fs_specific_has() parses the fs_specific string for a particular key, 
-   returns true if found, and optionally returns a val if the string is
-   of the form key=val.
- *
- * The fs_specific string is a comma (,) separated options where each option
- * can be of the form key=value or just key. Example:
- *	FS_specific = "foo=baz,enable_A";
- */
-static bool fs_specific_has(const char *fs_specific, const char* key,
-			    char *val, int max_val_bytes)
-{
-	char *next_comma, *option;
-	bool ret;
-	char *fso_dup = NULL;
-
-	if (!fs_specific || !fs_specific[0])
-		return false;
-
-	fso_dup = strdup(fs_specific);
-	if (!fso_dup) {
-		LogCrit(COMPONENT_FSAL,"strdup(%s) failed", fs_specific);
-		return false;
-	}
-
-	for (option = strtok_r(fso_dup, ",", &next_comma);
-	     option; 
-	     option=strtok_r(NULL, ",", &next_comma)) {
-		char *k = option;
-		char *v = k;
-
-		strsep(&v, "=");
-		if (0 == strcmp(k, key)) {
-			if(val)
-				strncpy(val, v, max_val_bytes);
-			ret = true;
-			goto cleanup;
-		}
-	}
-
-	ret = false;
-cleanup:
-	free(fso_dup);
-	return ret;
-}
 
 /* create_export
  * Create an export point and return a handle to it to be kept
