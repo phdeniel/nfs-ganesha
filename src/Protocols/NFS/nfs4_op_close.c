42b1889c8918b9faa7c624034c793208d29c218d
Philippe DENIEL <philippe.deniel@cea.fr>
2017-03-06 13:08:36 +0100

    FSAL_KVSFS: do not call cache_inode_invalidate_entry() at the end of nfs4_op_close()
    Change-Id: I5e2630de66d59a362f2b3470c61c83343efb9119
    Signed-off-by: Philippe DENIEL <philippe.deniel@cea.fr>

--- src/Protocols/NFS/nfs4_op_close.c
+++ src/Protocols/NFS/nfs4_op_close.c
@@ -319,9 +319,12 @@ int nfs4_op_close(struct nfs_argop4 *op, compound_data_t *data,
 		nfs4_owner_PrintAll();
 	}
 
+#if 0 /* This may mess up some cthon test */
 	/* For pNFS (coherency in-between DS and MDS: invalidate cache */
 	cache_inode_invalidate(data->current_entry,
 			       CACHE_INODE_INVALIDATE_ATTRS);
+#endif
+
 out:
 
 	/* Save the response in the open owner */
